bbbaae011f8803a1cceba6a3eadbd3e6146fb5b1
Greg Chance <ge.chance@gmail.com>
2020-03-16 23:38:50 -0700

    initial commit

---
+++ server/routes/routes.js
@@ -0,0 +1,8 @@
+const ProductController = require('../controllers/controller')
+
+module.exports = app => {
+    app.get('/api/index', ProductController.index)
+    app.get('/api/products', ProductController.readAll)
+    app.get('/api/products/:id', ProductController.readOne)
+    app.post('/api/products/new', ProductController.create)
+}
